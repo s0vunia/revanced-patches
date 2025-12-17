085bbf9662774b249c0a69fa917259150bc5261c
Loh <burindima324@gmail.com>
2025-12-17 10:27:04 +0300

    fix: use original build.gradle.kts to avoid plugin conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
 plugins {
-    kotlin("jvm") version "1.9.22" apply false
-    id("com.android.library") version "8.2.0" apply false
-}
-
-subprojects {
-    group = "app.revanced"
-    version = "1.0.0"
+    alias(libs.plugins.android.library) apply false
 }
